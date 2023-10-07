a87ff723cfc6f9ad028d1140002fe44edb6871aa
joberthdavid <joberthdavid@hotmail.com>
2023-10-07 18:28:25 -0300

    Remodelagem de entidades

--- core/models.py
+++ core/models.py
@@ -5,8 +5,9 @@ from django.core.validators import FileExtensionValidator
 
 from core.usefuls.choices import *
 
+import hashlib
 
-class ModelFileCost(models.Model):
+class SourceFile(models.Model):
 
     methodology = models.CharField(
         verbose_name="Metodologia",
@@ -18,10 +19,10 @@ class ModelFileCost(models.Model):
         verbose_name="Data-base",
         auto_now=False, auto_now_add=False
         )
-    file = models.FileField(
-        verbose_name="Arquivo PDF",
-        upload_to='core/pdf_uploaded/',
-        validators=[FileExtensionValidator(['pdf'])]
+    source_file = models.FileField(
+        verbose_name="Arquivo de origem",
+        upload_to='core/file_uploaded/',
+        validators=[FileExtensionValidator(['pdf', 'xlsx'])]
         )
     uf = models.CharField(
         verbose_name="UF",
@@ -33,23 +34,34 @@ class ModelFileCost(models.Model):
         verbose_name="Tipo de sistema",
         max_length=2,
         choices=TYPE_SYSTEM,
-        default=ONERADO,
+        default=NAO_APLICAVEL,
         )
     type_file = models.CharField(
         verbose_name="Tipo de arquivo",
         max_length=2,
         choices=FILE,
-        default=ANALITICO,
+        default=ONERADO,
         )
     status = models.BooleanField(
         verbose_name="Arquivo processado",
         default=False,
     )
-
+    number_of_lines_to_skip = models.IntegerField(
+        verbose_name="Número de linhas de cabeçalho",
+        blank=True,
+        null=True,
+        default=0,
+    )
 
     class Meta:
-        verbose_name="Arquivo de custo"
-        verbose_name_plural="Arquivos de custos"
+        verbose_name="Arquivo de origem"
+        verbose_name_plural="Arquivos de origem"
+        constraints = [
+            models.UniqueConstraint(
+               fields=['methodology', 'data_base', 'type_system','type_file'],
+               name="unique_file"
+            )
+        ]
 
     def __str__(self):
         return " - ".join([self.methodology, self.uf, self.parser_data_base_to_string(), self.type_system, self.type_file])
@@ -64,7 +76,7 @@ class ModelFileCost(models.Model):
         return '/scraper/%i/' % self.pk
     
 
-class ModelComposition(models.Model):
+class Composition(models.Model):
 
     composition_code = models.CharField(
         verbose_name="Código",
@@ -81,9 +93,9 @@ class ModelComposition(models.Model):
         max_digits=18,
         decimal_places=5
         )
-    file_cost = models.ForeignKey(
-        ModelFileCost,
-        verbose_name='Arquivo relacionado',
+    source_file = models.ForeignKey(
+        SourceFile,
+        verbose_name='Arquivo de origem',
         on_delete=models.CASCADE,
         default=None,
         null=True,
@@ -94,24 +106,23 @@ class ModelComposition(models.Model):
         max_length=2,
         choices=COMPOSITION_GROUP,
         )
-    status = models.BooleanField(
-        verbose_name="Composição validada",
-        default=False,
-    )
+
 
     class Meta:
         verbose_name="Composição"
         verbose_name_plural="Composições"
+        constraints = [
+            models.UniqueConstraint(
+               fields=['composition_code', 'fic', 'production', 'source_file'],
+               name="unique_composition"
+            )
+        ]
 
     def __str__(self):
         return str(self.composition_code)
-    
-    def update_group(self):
-        self.main_composition_group = self.__str__()[0:2]
-        self.save()
 
 
-class ModelInput(models.Model):
+class InputItem(models.Model):
 
     main_input_code = models.CharField(
         verbose_name="Código",
@@ -144,8 +155,8 @@ class ModelInput(models.Model):
 
         )
     related_composition = models.ForeignKey(
-        ModelComposition,
-        verbose_name='Composição relacionada',
+        Composition,
+        verbose_name='Composição proprietária',
         on_delete=models.CASCADE,
         default=None,
         null=True,
@@ -155,17 +166,103 @@ class ModelInput(models.Model):
     class Meta:
         verbose_name="Apropriação"
         verbose_name_plural="Apropriações"
+        constraints = [
+            models.UniqueConstraint(
+               fields=['main_input_code', 'main_input_quantity', 'main_input_use', 'transported_input_code', 'related_composition'],
+               name="unique_input"
+            )
+        ]
 
     def __str__(self):
         return str(self.related_composition) + " - " + str(self.main_input_code)
 
 
-class CompositionStamp:
+class Unit(models.Model):
 
-    def __init__( self ) -> None:
-        self.fic = Decimal(0.0)
-        self.data_base = datetime.date(1900,1,1)
-        self.production = Decimal(0.0)
-        self.unit = ''
-        self.composition_code = ''       
-        self.stop_flag = False
+    unit = models.CharField(
+        verbose_name="Unidade",
+        max_length=10,
+        default=None,
+        )
+    
+    def __str__(self):
+        return str(self.unit)
+
+class GenericItem(models.Model):
+    
+    code = models.CharField(
+        verbose_name="Código",
+        max_length=20,
+        primary_key=True,
+        unique=True,
+        blank=False,
+        editable=False,
+        )
+    unit = models.ForeignKey(
+        Unit,
+        verbose_name='Unidade',
+        on_delete=models.CASCADE,
+        default=None,
+        null=True,
+        blank=True,
+        )
+    source_files = models.ManyToManyField(
+        SourceFile,
+        verbose_name='Origens',
+        default=None,
+        blank=True,
+        )
+    group = models.CharField(
+        verbose_name="Grupo",
+        max_length=2,
+        choices=GENERIC_GROUP,
+        default=None,
+        )
+
+    class Meta:
+        verbose_name="Item genérico"
+        verbose_name_plural="Itens genéricos"
+        constraints = [
+            models.UniqueConstraint(
+               fields=['code', 'unit'],
+               name="unique_generic_item"
+            )
+        ]
+
+    def __str__(self):
+        return str(self.code)
+
+
+class GenericDescription(models.Model):
+
+    generic_item = models.ForeignKey(
+        GenericItem,
+        verbose_name="Código",
+        on_delete=models.CASCADE,
+        default=None,
+        null=True,
+        blank=True,
+        )
+    description = models.TextField(
+        verbose_name="Descrição",
+        unique=True,
+        )
+    source_files = models.ManyToManyField(
+        SourceFile,
+        verbose_name='Origens',
+        default=None,
+        blank=True,
+        )
+
+    class Meta:
+        verbose_name="Descrição genérica"
+        verbose_name_plural="Descrições genéricas"
+        constraints = [
+            models.UniqueConstraint(
+               fields=['description',],
+               name="unique_generic_description"
+            )
+        ]
+
+    def __str__(self):
+        return str(self.generic_item) + " - " + str(self.description)
